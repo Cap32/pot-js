e97dc2d476e8866dcedf5d5d9a679dda55ff4f4b
3went2 <webb.png@gmail.com>
2016-12-29 18:47:56 +0800

    update test server

--- test/server.js
+++ test/server.js
@@ -1,12 +1,5 @@
 
-process.on('message', (buf) => {
-
-	// eslint-disable-next-line
-	const config = eval(`(${buf.toString()})`);
-
-	require('http').createServer((req, res) => {
-		console.log('[HTTP]', req.url);
-		res.end('hello, pot-js');
-	}).listen(3000);
-
-});
+require('http').createServer((req, res) => {
+	console.log('[HTTP]', req.url);
+	res.end('hello, pot-js');
+}).listen(3000);
