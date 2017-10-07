0fa2d62d1df9ff9238b6f029cb680cba2b3d588a
Webb Peng <webb.png@gmail.com>
2017-10-07 23:03:00 +0800

    expose schema to API

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ export stop from './stop';
 export list from './list';
 export log from './log';
 export dir from './dir';
-export * from './utils/serialize';
 export Bridge from './Bridge';
 export workspace from './utils/workspace';
 export resolveConfig from './utils/resolveConfig';
+export schema from './utils/schema';
