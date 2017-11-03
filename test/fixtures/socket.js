2ad85a10f5fe58292c97c8b70b693f2a7571bc25
3went2 <webb.png@gmail.com>
2017-11-03 18:52:34 +0800

    update testing

--- test/fixtures/socket.js
+++ test/fixtures/socket.js
@@ -1,23 +1,18 @@
 
-const { PORT = 3000, POT_TESTING } = process.env;
-
-const { Server } = require('promise-ws');
-
-(async function () {
-	try {
-		const server = await Server.create({ port: PORT });
-
-		server.on('test', async (data) => {
+require('promise-ws').Server
+	.create({ port: process.env.PORT || 3000 })
+	.then((server) => {
+		server.on('test', (data) => {
 			return data;
 		});
 
-		server.on('env', async () => {
-			return POT_TESTING;
+		server.on('env', () => {
+			return process.env.POT_TESTING;
 		});
 
 		console.log('socket server started');
-	}
-	catch (err) {
+	})
+	.catch((err) => {
 		console.error(err);
-	}
-}());
+	})
+;
