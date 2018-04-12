642e7f7a2c3af6d2a184008b30acd80080778025
Webb Peng <webb.png@gmail.com>
2018-04-13 01:55:05 +0800

    add Commands

---
+++ src/Commands.js
@@ -0,0 +1,72 @@
+import { logger, setLoggers } from 'pot-logger';
+import resolveConfig from './utils/resolveConfig';
+import * as Schemas from './schemas/cli';
+import * as Handlers from './';
+
+export const start = {
+	command: 'start [entry]',
+	description: 'Start process',
+	builder: (yargs) => {
+		const { entry, ...options } = Schemas.start.properties;
+		return yargs
+			.usage('$0 start [entry] [options]')
+			.positional('entry', entry)
+			.options(options).argv;
+	},
+	async handler(argv) {
+		try {
+			await Handlers.start(await resolveConfig(argv));
+		}
+		catch (err) {
+			setLoggers('logLevel', argv.logLevel);
+			logger.error(err.message);
+			logger.debug(err);
+		}
+	},
+};
+
+export const stop = {
+	command: 'stop [name]',
+	description: 'Stop process',
+	builder: (yargs) => yargs.options(Schemas.stop.properties).argv,
+	handler(argv) {
+		Handlers.stop(argv).catch((err) => logger.error(err.message));
+	},
+};
+
+export const stopall = {
+	command: 'stopall',
+	description: 'Stop all processes',
+	builder: (yargs) => yargs.options(Schemas.stop.properties).argv,
+	handler(argv) {
+		Handlers.stopAll(argv).catch((err) => logger.error(err.message));
+	},
+};
+
+export const list = {
+	command: 'list',
+	aliases: ['ls'],
+	description: 'List processes',
+	builder: (yargs) => yargs.options(Schemas.list.properties).argv,
+	handler(argv) {
+		Handlers.list(argv).catch((err) => logger.error(err.message));
+	},
+};
+
+export const log = {
+	command: 'log [name]',
+	description: 'Show log',
+	builder: (yargs) => yargs.options(Schemas.log.properties).argv,
+	handler(argv) {
+		Handlers.log(argv).catch((err) => logger.error(err.message));
+	},
+};
+
+export const dir = {
+	command: 'dir [name]',
+	description: 'Show dir',
+	builder: (yargs) => yargs.options(Schemas.dir.properties).argv,
+	handler(argv) {
+		Handlers.dir(argv).catch((err) => logger.error(err.message));
+	},
+};
--- src/cli.js
+++ src/cli.js
@@ -1,77 +1,18 @@
 import yargs from 'yargs';
 import { name, version } from '../package.json';
 import { upperCase } from 'lodash';
-import { logger, setLoggers } from 'pot-logger';
-import resolveConfig from './utils/resolveConfig';
-import * as Schemas from './schemas/cli';
-import { start, stop, stopAll, list, log, dir } from './';
+import * as Commands from './Commands';
 
 // eslint-disable-next-line
 yargs
 	.usage('$0 <command> [options]')
 	.demand(1, 'Please specify one of the commands!')
-	.command({
-		command: 'start [entry]',
-		description: 'Start process',
-		builder: (yargs) => {
-			const { entry, ...options } = Schemas.start.properties;
-			return yargs
-				.usage('$0 start [entry] [options]')
-				.positional('entry', entry)
-				.options(options).argv;
-		},
-		async handler(argv) {
-			try {
-				await start(await resolveConfig(argv));
-			}
-			catch (err) {
-				setLoggers('logLevel', argv.logLevel);
-				logger.error(err.message);
-				logger.debug(err);
-			}
-		},
-	})
-	.command({
-		command: 'stop [name]',
-		description: 'Stop process',
-		builder: (yargs) => yargs.options(Schemas.stop.properties).argv,
-		handler(argv) {
-			stop(argv).catch((err) => logger.error(err.message));
-		},
-	})
-	.command({
-		command: 'stopall',
-		description: 'Stop all processes',
-		builder: (yargs) => yargs.options(Schemas.stop.properties).argv,
-		handler(argv) {
-			stopAll(argv).catch((err) => logger.error(err.message));
-		},
-	})
-	.command({
-		command: 'list',
-		aliases: ['ls'],
-		description: 'List processes',
-		builder: (yargs) => yargs.options(Schemas.list.properties).argv,
-		handler(argv) {
-			list(argv).catch((err) => logger.error(err.message));
-		},
-	})
-	.command({
-		command: 'log [name]',
-		description: 'Show log',
-		builder: (yargs) => yargs.options(Schemas.log.properties).argv,
-		handler(argv) {
-			log(argv).catch((err) => logger.error(err.message));
-		},
-	})
-	.command({
-		command: 'dir [name]',
-		description: 'Show dir',
-		builder: (yargs) => yargs.options(Schemas.dir.properties).argv,
-		handler(argv) {
-			dir(argv).catch((err) => logger.error(err.message));
-		},
-	})
+	.command(Commands.start)
+	.command(Commands.stop)
+	.command(Commands.stopall)
+	.command(Commands.list)
+	.command(Commands.log)
+	.command(Commands.dir)
 	.env(upperCase(name))
 	.alias('h', 'help')
 	.wrap(yargs.terminalWidth())
