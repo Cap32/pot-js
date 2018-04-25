5e98a97acfeb365b65dbd145efee739676f4b79d
3went2 <webb.png@gmail.com>
2018-04-25 12:48:59 +0800

    enhance createCli

--- src/cli.js
+++ src/cli.js
@@ -1,16 +1,5 @@
-import { version } from '../package.json';
+import pkg from '../package.json';
 import createCli from './utils/createCli';
 import * as Commands from './Commands';
 
-createCli(
-	(yargs) =>
-		yargs
-			.usage('$0 <command> [options]')
-			.demand(1, 'Please specify one of the commands!')
-			.alias('h', 'help')
-			.help()
-			.version(version)
-			.env('POT')
-			.recommendCommands(),
-	Commands,
-);
+createCli(pkg, Commands);
--- src/utils/createCli.js
+++ src/utils/createCli.js
@@ -1,9 +1,39 @@
 import createCommand from './createCommand';
 import yargs from 'yargs';
-import { isObject, map } from 'lodash';
+import { isObject, isFunction, map } from 'lodash';
 
 export default function createCli(getYargs, commands, options = {}) {
-	const instance = getYargs(yargs);
+	const instance = (function () {
+		if (isObject(getYargs) && getYargs.version) {
+			const pkg = getYargs;
+			getYargs = (yargs) =>
+				yargs
+					.usage('$0 <command> [options]')
+					.demand(1, 'Please specify one of the commands!')
+					.alias('h', 'help')
+					.help()
+					.version(pkg.version)
+					.env(pkg.name.toUpperCase())
+					.recommendCommands();
+		}
+
+		if (isFunction(getYargs)) {
+			return getYargs(yargs);
+		}
+		else if (isObject(getYargs) && getYargs.name && getYargs.version) {
+			const pkg = getYargs;
+			return yargs
+				.usage('$0 <command> [options]')
+				.demand(1, 'Please specify one of the commands!')
+				.alias('h', 'help')
+				.help()
+				.version(pkg.version)
+				.env(pkg.name.toUpperCase())
+				.recommendCommands();
+		}
+		return yargs;
+	})();
+
 	if (!Array.isArray(commands) && isObject(commands)) {
 		commands = map(commands);
 	}
