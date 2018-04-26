eaefa2faafd4c9e32c53a4723af3aa9594bcde59
3went2 <webb.png@gmail.com>
2018-04-26 12:29:25 +0800

    fix scale() bug

--- src/scale.js
+++ src/scale.js
@@ -4,7 +4,7 @@ import { scale as schema } from './schemas/cli';
 
 export default async function scale(options = {}) {
 	prepareRun(schema, options);
-	const { connection, targetName } = await prepareTarget();
+	const { connection, targetName } = await prepareTarget(options);
 	const { ok } = await connection.scale(options.instances);
 	if (ok) {
 		logger.info(`"${targetName}" scale compeleted`);
