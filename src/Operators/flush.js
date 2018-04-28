0c01d4b63ed52d851672efb148c453b3e6be1d6b
3went2 <webb.png@gmail.com>
2018-04-28 14:39:00 +0800

    enhance flushall command

--- src/Operators/flush.js
+++ src/Operators/flush.js
@@ -2,6 +2,28 @@ import { logger, flush as flushLogFiles } from 'pot-logger';
 import Connection from '../Connection';
 import { prepareRun, prepareTarget } from '../utils/PrepareCli';
 import { flush as schema } from '../schemas/cli';
+import globby from 'globby';
+import workspace from '../utils/workspace';
+import { difference } from 'lodash';
+import { join } from 'path';
+
+const flushOfflineDirs = async function flushOfflineDirs(onlineNames) {
+	try {
+		const cwd = await workspace.getLogsDir();
+		const all = await globby('*', {
+			onlyDirectories: true,
+			cwd,
+		});
+		const offlines = difference(all, onlineNames);
+		await Promise.all(
+			offlines.map(async (name) => {
+				const logsDir = join(cwd, name);
+				return flushLogFiles({ logsDir, removeDir: true });
+			}),
+		);
+	}
+	catch (err) {}
+};
 
 export const flush = async function flush(options = {}) {
 	prepareRun(schema, options);
@@ -19,5 +41,6 @@ export const flush = async function flush(options = {}) {
 
 export const flushAll = async function flushAll(options = {}) {
 	const names = await Connection.getNames();
+	flushOfflineDirs(names);
 	return Promise.all(names.map(async (name) => flush({ ...options, name })));
 };
