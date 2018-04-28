4e30be8d34e7151189e6756200d1411ea70e5917
3went2 <webb.png@gmail.com>
2018-04-28 15:24:20 +0800

    optimze createCli

--- src/utils/createCli.js
+++ src/utils/createCli.js
@@ -3,24 +3,8 @@ import yargs from 'yargs';
 import { isObject, isFunction, map } from 'lodash';
 
 export default function createCli(getYargs, commands, options = {}) {
-	const instance = (function () {
-		if (isObject(getYargs) && getYargs.version) {
-			const pkg = getYargs;
-			getYargs = (yargs) =>
-				yargs
-					.usage('$0 <command> [options]')
-					.demand(1, 'Please specify one of the commands!')
-					.alias('h', 'help')
-					.help()
-					.version(pkg.version)
-					.env(pkg.name.toUpperCase())
-					.recommendCommands();
-		}
-
-		if (isFunction(getYargs)) {
-			return getYargs(yargs);
-		}
-		else if (isObject(getYargs) && getYargs.name && getYargs.version) {
+	const args = (function () {
+		if (isObject(getYargs) && getYargs.name && getYargs.version) {
 			const pkg = getYargs;
 			return yargs
 				.usage('$0 <command> [options]')
@@ -31,7 +15,9 @@ export default function createCli(getYargs, commands, options = {}) {
 				.env(pkg.name.toUpperCase())
 				.recommendCommands();
 		}
-		return yargs;
+		else if (isFunction(getYargs)) {
+			return getYargs(yargs);
+		}
 	})();
 
 	if (!Array.isArray(commands) && isObject(commands)) {
@@ -39,7 +25,7 @@ export default function createCli(getYargs, commands, options = {}) {
 	}
 	if (Array.isArray(commands)) {
 		commands.forEach((localOptions) =>
-			instance.command(
+			args.command(
 				createCommand({
 					...options,
 					...localOptions,
@@ -47,5 +33,5 @@ export default function createCli(getYargs, commands, options = {}) {
 			),
 		);
 	}
-	return instance.argv;
+	return args.argv;
 }
