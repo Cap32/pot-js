a8d4fbadfe30c2fd5c3c278cc201121a62d20c0a
3went2 <webb.png@gmail.com>
2018-08-09 17:54:42 +0800

    update nssocketPromise

--- src/utils/nssocketPromise.js
+++ src/utils/nssocketPromise.js
@@ -1,9 +1,52 @@
 import nssocket from 'nssocket';
 import delay from 'delay';
 import createLocalDomainSocket from 'create-local-domain-socket';
+import { REQUEST, PUBLISH } from './SocketEventTypes';
+import { logger } from 'pot-logger';
+import EventEmitter from 'events';
 
 export async function createServer(portOrPath, connect) {
 	const server = nssocket.createServer((socket) => {
+		const reqRepBus = new EventEmitter();
+		const pubSubBus = new EventEmitter();
+
+		socket.data(REQUEST, (data) => {
+			if (data && data.event) {
+				reqRepBus.emit(data.event, ...data.args);
+			}
+		});
+
+		socket.data(PUBLISH, (data) => {
+			if (data && data.event) {
+				pubSubBus.emit(data.event, ...data.args);
+			}
+		});
+
+		socket.rep = async function rep(event, handler) {
+			reqRepBus.on(event, async (...args) => {
+				try {
+					const res = await handler(...args);
+					socket.send(event, res);
+				}
+				catch (err) {
+					logger.error(`Failed to reply "${event}"`);
+					socket.emit('error', err);
+				}
+			});
+		};
+
+		socket.sub = async function sub(event, handler) {
+			pubSubBus.on(event, async (...args) => {
+				try {
+					await handler(...args);
+				}
+				catch (err) {
+					logger.error(`Failed to subscribe "${event}"`);
+					socket.emit('error', err);
+				}
+			});
+		};
+
 		connect(socket);
 	});
 	if (/^\d/.test(portOrPath)) {
@@ -27,6 +70,20 @@ export async function createClient(portOrPath, timeout = 30000) {
 
 	const createClientPromise = (function () {
 		const client = new nssocket.NsSocket();
+
+		client.req = (event, ...args) => {
+			const data = { event, args };
+			client.send(REQUEST, data);
+			return new Promise((resolve) => {
+				client.dataOnce(event, resolve);
+			});
+		};
+
+		client.pub = (event, ...args) => {
+			const data = { event, args };
+			client.send(PUBLISH, data);
+		};
+
 		return new Promise((resolve, reject) => {
 			client.on('start', () => resolve(client));
 			client.on('error', reject);
