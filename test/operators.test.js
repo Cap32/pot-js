d9664326b04cfbdda01aebadec9bf00ab69a2b3e
Webb Peng <webb.png@gmail.com>
2018-08-12 00:01:06 +0800

    add operators.restart testing

--- test/operators.test.js
+++ test/operators.test.js
@@ -52,4 +52,47 @@ describe('operators', () => {
 			expect(potNames.length).toBe(0);
 		});
 	});
+
+	describe('operator.restart', () => {
+		test('should operator.restart work', async () => {
+			const pot = await Operators.start({
+				name: 'foo',
+				entry: 'test/fixtures/timeout.js',
+			});
+			pots.add(pot);
+			const state0 = await pot.getState();
+			const t0 = state0.monitor.started;
+			await Operators.restart({ name: 'foo' });
+			const state1 = await pot.getState();
+			const t1 = state1.monitor.started;
+			expect(t1 > t0).toBe(true);
+		});
+
+		test('should operator.stopAll work', async () => {
+			const names = ['foo', 'bar'];
+			const newPots = await Promise.all(
+				names.map((name) =>
+					Operators.start({
+						name,
+						entry: 'test/fixtures/timeout.js',
+					}),
+				),
+			);
+			const ts0 = [];
+			const ts1 = [];
+			for (const newPot of newPots) {
+				pots.add(newPot);
+				const state = await newPot.getState();
+				ts0.push(state.monitor.started);
+			}
+			await Operators.restartAll();
+			for (const newPot of newPots) {
+				const state = await newPot.getState();
+				ts1.push(state.monitor.started);
+			}
+			names.forEach((_, index) => {
+				expect(ts1[index] > ts0[index]).toBe(true);
+			});
+		});
+	});
 });
